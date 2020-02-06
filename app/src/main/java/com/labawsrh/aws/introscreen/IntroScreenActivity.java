0603a1c3773dbb1ee44391f90fabbbad7a0636b0
Chris <chrispius99@gmail.com>
2020-02-06 16:56:53 +0530

    Splash Screen + Intro Sliders

---
+++ app/src/main/java/com/labawsrh/aws/introscreen/IntroScreenActivity.java
@@ -0,0 +1,14 @@
+package com.labawsrh.aws.introscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
